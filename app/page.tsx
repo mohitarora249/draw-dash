71ea77cedd6c31b97222bf875c1469676574d47c
mohitarora249 <mohitarora249@gmail.com>
2024-03-12 20:19:17 +0530

    redirection from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Header from "./_components/header";
 import MainContent from "./_components/main-content";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isAuth = await isAuthenticated();
+
+  if (isAuth) redirect("/dashboard");
+
   return (
     <main className="h-screen w-screen mx-auto max-w-screen-xl px-4 flex flex-col">
       <Header />
