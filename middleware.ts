a42817106a78ec443ae66dca0ad79c031afaa336
mohitarora249 <mohitarora249@gmail.com>
2024-03-12 19:35:48 +0530

    middleware updates for protecting routes

--- middleware.ts
+++ middleware.ts
@@ -13,5 +13,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard", "/teams/:path*", "/workspace/:path*"],
 };
