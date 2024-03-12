50810bd3b68390e3fdab308da6efe629d7c68e1f
mohitarora249 <mohitarora249@gmail.com>
2024-03-12 22:26:01 +0530

    landing page updates

--- app/_components/main-content.tsx
+++ app/_components/main-content.tsx
@@ -1,24 +1,44 @@
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import TypewriterEffect from "./type-writer-effect";
 
 const MainContent = () => {
   return (
     <section className="flex h-full flex-col items-center justify-center">
-      <section className="mx-auto max-w-3xl text-center">
-        <h1 className="text-black text-3xl font-extrabold sm:text-5xl">
-          Documents & Diagrams
-          <span className="text-blue-500 sm:block"> for Developers </span>
-        </h1>
-        <div className="mt-8 flex flex-wrap justify-center gap-4">
-          <Link className={buttonVariants({ variant: "secondary" })} href="#">
-            Explore Features
-          </Link>
-          <Link className={buttonVariants({})} href="#">
-            Learn More
-          </Link>
-        </div>
-      </section>
+      <TypewriterEffect
+        className="tracking-wider"
+        words={[
+          {
+            text: "Notes",
+            className: "text-3xl md:text-[5rem]",
+          },
+          {
+            text: "&",
+            className: "text-3xl md:text-[5rem]",
+          },
+          {
+            text: "Sketches",
+            className: "text-3xl md:text-[5rem]",
+          },
+          {
+            text: "for",
+            className: "text-3xl md:text-[5rem]",
+          },
+          {
+            text: "Developers",
+            className: "text-3xl md:text-[5rem] text-blue-500",
+          },
+        ]}
+      />
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+        <Link className={buttonVariants({ variant: "secondary" })} href="#">
+          Explore Features
+        </Link>
+        <Link className={buttonVariants({})} href="#">
+          Learn More
+        </Link>
+      </div>
     </section>
   );
 };
