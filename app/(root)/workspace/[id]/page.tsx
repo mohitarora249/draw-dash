77f677aba5a7fa31dd3c8b5d4db40149fdd0a94d
mohitarora249 <mohitarora249@gmail.com>
2024-03-13 19:51:41 +0530

    added header on workspace page

--- app/(root)/workspace/[id]/page.tsx
+++ app/(root)/workspace/[id]/page.tsx
@@ -3,31 +3,46 @@ import Canvas from "../_components/canvas";
 import Editor from "../_components/editor";
 import Header from "../_components/header";
 import useFile from "@/app/_hooks/use-file";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 const Workspace = ({ params }: any) => {
   const { fileData, setTriggerSave, triggerSave } = useFile({ id: params.id });
-
+  const [tab, setTab] = useState<"document" | "both" | "canvas">("both");
+  const onTabChangeHandler = (t: "document" | "both" | "canvas") => {
+    setTab(t);
+  };
   return (
     <div className="flex flex-col h-screen w-full">
       <Header
+        onTabChangeHandler={onTabChangeHandler}
         fileName={fileData?.fileName}
         onSave={() => setTriggerSave(!triggerSave)}
       />
-      <div className="flex-1 h-full grid grid-cols-1 md:grid-cols-2">
-        <div className="h-full">
-          <Editor
-            onSaveTrigger={triggerSave}
-            fileId={params.id}
-            fileData={fileData}
-          />
-        </div>
-        <div className="h-full border-l">
-          <Canvas
-            onSaveTrigger={triggerSave}
-            fileId={params.id}
-            fileData={fileData}
-          />
-        </div>
+      <div
+        className={cn(
+          "flex-1 h-full",
+          tab === "both" && "grid grid-cols-1 md:grid-cols-2"
+        )}
+      >
+        {(tab === "both" || tab === "document") && (
+          <div className="h-full w-full">
+            <Editor
+              onSaveTrigger={triggerSave}
+              fileId={params.id}
+              fileData={fileData}
+            />
+          </div>
+        )}
+        {(tab === "both" || tab === "canvas") && (
+          <div className={cn("h-full w-full", tab === "both" && "border-l")}>
+            <Canvas
+              onSaveTrigger={triggerSave}
+              fileId={params.id}
+              fileData={fileData}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
--- app/(root)/workspace/_components/header.tsx
+++ app/(root)/workspace/_components/header.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Link, Save } from "lucide-react";
-import React from "react";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const Header = ({ onSave, fileName }: any) => {
+const Header = ({ onSave, fileName, onTabChangeHandler }: any) => {
   return (
     <div className="p-3 border-b flex justify-between items-center">
       <div className="flex gap-2 items-center">
@@ -10,8 +10,27 @@ const Header = ({ onSave, fileName }: any) => {
           <span>D</span>
           <span className="text-blue-500">D</span>
         </div>
-        <h2>{fileName}</h2>
+        <h2 className="font-semibold text-xl">{fileName}</h2>
       </div>
+      <Tabs defaultValue="both" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger
+            value="document"
+            onClick={() => onTabChangeHandler("document")}
+          >
+            Document
+          </TabsTrigger>
+          <TabsTrigger value="both" onClick={() => onTabChangeHandler("both")}>
+            Both
+          </TabsTrigger>
+          <TabsTrigger
+            value="canvas"
+            onClick={() => onTabChangeHandler("canvas")}
+          >
+            Canvas
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
       <div className="flex items-center gap-4">
         <Button className="gap-2" onClick={onSave}>
           <Save className="h-4 w-4" />
